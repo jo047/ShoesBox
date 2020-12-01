6ce148e9cb23d241578f4b1e5bc22c0dac052d81
EC2 Default User <ec2-user@ip-172-31-73-245.ec2.internal>
2020-12-01 12:28:19 +0000

    [add]検索機能，いいね非同期

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_search
 
   def after_sign_up_path_for(resource)
     shoes_posts_path(resource)
@@ -8,6 +9,13 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     shoes_posts_path(resource)
   end
+
+  def set_search
+    #@search = Article.search(params[:q])
+    @search = ShoesPost.ransack(params[:q]) #ransackメソッド推奨
+    @search_shoes_posts = @search.result.page(params[:page]).reverse_order
+  end
+
 
   protected
   def configure_permitted_parameters
--- app/controllers/shoes_comments_controller.rb
+++ app/controllers/shoes_comments_controller.rb
@@ -5,12 +5,12 @@ class ShoesCommentsController < ApplicationController
     comment = current_user.shoes_comments.new(shoes_comment_params)
     comment.shoes_post_id = shoes_post.id
     comment.save
-    redirect_to shoes_post_path(shoes_post)
+    # redirect_to shoes_post_path(shoes_post)
   end
 
   def destroy
     ShoesComment.find_by(id: params[:id], shoes_post_id: params[:shoes_post_id]).destroy
-    redirect_to shoes_post_path(params[:shoes_post_id])
+    # redirect_to shoes_post_path(params[:shoes_post_id])
   end
 
   private
